fbac4ec4b53dce360101a4d4b3a3b20e407b633b
yv0vaa <zakharov.2004@yandex.ru>
2024-03-18 19:14:59 +0300

    little updates

--- src/game_engine/scenes/game_scene/SceneSetup.py
+++ src/game_engine/scenes/game_scene/SceneSetup.py
@@ -1,9 +1,11 @@
+import random
+
 from src.game_engine.controllers.Controller import *
 from src.game_engine.entities.ObjectFactory import *
 from src.game_engine.scenes.game_scene.EnvGeneration import ReadPositions
 
 
-def SceneSetup(scene, cone_count=10, car_count=10, rotated_car_count=10):
+def SceneSetup(scene):
     trees_positions, cones_positions, cars_positions, \
         rotated_cars_positions, vert_barrier_positions, hor_barriers_positions = ReadPositions()
     for position in set(trees_positions):
@@ -14,7 +16,7 @@ def SceneSetup(scene, cone_count=10, car_count=10, rotated_car_count=10):
             position=position,
             static_obstacle_model='tree'
         )
-    for position in set(random.choices(cones_positions, k=cone_count)):
+    for position in set(random.choices(cones_positions, k=random.randint(10, 40))):
         scene.traffic_cones.append(
             ObjectFactory.create_object(
                 render_group=scene.render_group,
@@ -24,7 +26,7 @@ def SceneSetup(scene, cone_count=10, car_count=10, rotated_car_count=10):
                 movable_obstacle_model='cone'
             )
         )
-    for position in set(random.choices(rotated_cars_positions, k=rotated_car_count)):
+    for position in set(random.choices(rotated_cars_positions, k=random.randint(5, 10))):
         car = ObjectFactory.create_object(render_group=scene.render_group,
                                           space=scene.space,
                                           object_type='car',
@@ -33,7 +35,7 @@ def SceneSetup(scene, cone_count=10, car_count=10, rotated_car_count=10):
                                           angle=random.choice([90, -90]))
         car.switch_controller(random.choice([RandomController(), AIController(), BrakeController()]))
         scene.cars.append(car)
-    for position in set(random.choices(cars_positions, k=car_count)):
+    for position in set(random.choices(cars_positions, k=random.randint(5, 10))):
         car = ObjectFactory.create_object(render_group=scene.render_group,
                                           space=scene.space,
                                           object_type='car',
@@ -41,7 +43,7 @@ def SceneSetup(scene, cone_count=10, car_count=10, rotated_car_count=10):
                                           car_model='red_car')
         car.switch_controller(random.choice([RandomController(), AIController(), BrakeController()]))
         scene.cars.append(car)
-    for position in set(vert_barrier_positions):
+    for position in vert_barrier_positions:
         ObjectFactory.create_object(
             render_group=scene.render_group,
             space=scene.space,
@@ -50,7 +52,7 @@ def SceneSetup(scene, cone_count=10, car_count=10, rotated_car_count=10):
             angle=90,
             static_obstacle_model='metal_pipe'
         )
-    for position in set(hor_barriers_positions):
+    for position in hor_barriers_positions:
         ObjectFactory.create_object(
             render_group=scene.render_group,
             space=scene.space,
