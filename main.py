ef05a41d46bd19fb28c80dbf2a7c7c7878c5fc54
Artem Batygin <aa.batygin@gmail.com>
2024-05-18 11:52:04 +0300

    Another CI/CD fixes

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from src.game_engine.Core import Core
 from src.game_engine.controllers.TrainAI import Train
 
 
 if __name__ == '__main__':
-    # core = Core()
-    # core.run()
-
     train = Train()
     train.run()
